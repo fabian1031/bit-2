679235e8db9ffbc22d839334dfb735efe5691bbf
fabian <lugia759@gmail.com>
2025-05-26 22:02:27 -0500

    updaye project

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 'use strict';
-'use strict';
 fetch("notas.json")
     .then((res) => res.json()) 
     .then((info) => {
-        console.log(info); 
-        const data = info;
+        console.log(info);
         const lista = document.getElementById("lista-estudiantes");
 
-        for(let i = 0; i < info.length; i++) {
+        for (let i = 0; i < info.length; i++) {
             console.log(info[i].student);
 
-           
             const li = document.createElement("li");
             li.textContent = `Nombre: ${info[i].student}`;
+
+            // Crear imagen de GitHub
+            const img = document.createElement("img");
+            img.src = `https://github.com/${info[i].usernameGithub}.png`;
+            img.alt = `Avatar de ${info[i].student}`;
+            img.style.width = "50px";  
+            const link = document.createElement("a");
+            link.href = `https://github.com/${info[i].usernameGithub}`;
+            link.target = "_blank";
+            link.rel = "noopener noreferrer";
+            link.textContent = "GitHub";
+
+            
+            li.appendChild(img);
+            li.appendChild(link);
             lista.appendChild(li);
         }
     })
